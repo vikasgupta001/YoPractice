a777fd40ee0ed629ecc515e59b70906fa5f0f271
vikasgupta001 <cse.vikashgupta@gmail.com>
2017-07-28 17:37:12 +0530

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myFirstYoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myFirstYoApp
+ */
+angular.module('myFirstYoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myFirstYoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myFirstYoApp
+ */
+angular.module('myFirstYoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
